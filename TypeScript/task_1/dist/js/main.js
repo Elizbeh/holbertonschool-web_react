3a8103bc49e305d67b8d3a24c459c566fd66510f
root <root@LAPTOP-J7HTNKBB>
2024-01-03 15:47:58 +0100

    npx webpack

---
+++ TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,11 @@
+// task_1/js/main.ts
+// Example usage
+const teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+//# sourceMappingURL=main.js.map
