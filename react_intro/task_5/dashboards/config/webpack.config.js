c2fda6b23967c3f9b4e7b4c744f7ef26b9ed7e40
root <root@LAPTOP-J7HTNKBB>
2024-01-22 16:53:03 +0100

    8. Create a project using Webpack

---
+++ react_intro/task_5/dashboards/config/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
