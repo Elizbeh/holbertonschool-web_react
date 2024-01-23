500b222a484884102d11a2163537ea6be82931c8
root <root@LAPTOP-J7HTNKBB>
2024-01-23 10:44:07 +0100

    10. Reorganize the files

---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,19 @@
+// task_3/dashboard/src/Notifications.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+test('Notifications renders without crashing', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+test('Notifications renders three list items', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.find('li').length).toBe(3);
+});
+
+test('Notifications renders the text "Here is the list of notifications"', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.text()).toContain('Here is the list of notifications');
+});
