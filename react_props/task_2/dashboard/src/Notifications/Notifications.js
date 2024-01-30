c2e3bb0c64b1c32dbe434e5325552ba1e0126bae
root <root@LAPTOP-J7HTNKBB>
2024-01-30 15:36:16 +0100

    Test

--- react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import '../Notifications/Notifications.css';
+import '../../src/Notifications/Notifications.css';
 import { getLatestNotification } from '../utils/utils';
-import NotificationItem from '../Notifications/NotificationItem';
 
 function Notifications() {
-  return (
-    <div className='Notifications'>
-      <button
-        style={{
-          position: "absolute",
-          right: "16px",
-          top: ".8rem",
-          fontSize: "16px",
-          border: "none",
-          background: "none",
-          cursor: "pointer"
-        }}
-        aria-label="Close"
-        onClick={() => {
-          console.log("Close button has been clicked\n");
-        }}
-      >
-        x
-      </button>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <NotificationItem /* props for first item */ />
-        <NotificationItem /* props for second item */ />
-        <NotificationItem /* props for third item */ />
-      </ul>
-    </div>
-  );
+    return (
+        <div className='Notifications'>
+            <button
+                style={{
+                    position: "absolute",
+                    right: "16px",
+                    top: ".8rem",
+                    fontSize: "16px",
+                    border: "none",
+                    background: "none",
+                    cursor: "pointer"
+                }}
+                aria-label="Close"
+                onClick={ () => {
+                    console.log("Close button has been clicked\n");
+                }}
+            >x</button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+            </ul>
+        </div>
+    );
 }
 
 export default Notifications;
