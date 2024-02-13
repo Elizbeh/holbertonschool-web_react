1f76332fa3ef4564ed4ab89cb0a40dc7ecb4c0b4
root <root@LAPTOP-J7HTNKBB>
2024-02-13 13:21:38 +0100

    Lifecycles

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     super(props);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
-
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyPress);
   }
