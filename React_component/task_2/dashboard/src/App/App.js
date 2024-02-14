4fd28b01e8321a9bf5b2a87a62acef88ba633bb1
root <root@LAPTOP-J7HTNKBB>
2024-02-14 13:48:37 +0100

    2. Handling Events

---
+++ React_component/task_2/dashboard/src/App/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {
+    const { logOut } = this.props;
+
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <div>
+        <Notifications />
+        <Header />
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
---
+++ React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './Notifications.css';
+import NotificationItem from '../Notifications/NotificationItem';
+import { getLatestNotification } from '../utils/utils';
+
+class Notifications extends Component {
+  constructor(props) {
+    super();
+    this.markAsRead = this.markAsRead.bind(this)
+  }
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`)
+  }
+  render() {
+    const { displayDrawer } = this.props;
+
+    return (
+      <div className='container'>
+        <div className={`menuItem${displayDrawer ? ' display-menuItem' : ''}`}>
+          Your notifications
+        </div>
+        <div className={`Notifications${displayDrawer ? ' display-drawer' : ''}`}>
+          <button
+            style={{
+              position: 'absolute',
+              right: '16px',
+              top: '.8rem',
+              fontSize: '16px',
+              border: 'none',
+              background: 'none',
+              cursor: 'pointer',
+            }}
+            aria-label="Close"
+            onClick={() => {
+              console.log('Close button has been clicked\n');
+            }}
+          >
+            x
+          </button>
+          <p>Here is the list of notifications</p>
+          <ul>
+            <NotificationItem
+            id={1}
+            type="default"
+            value="New course available"
+             markAsRead={this.markAsRead}
+            />
+            <NotificationItem
+              id={2}
+              type="urgent"
+              value="Urgent requirement - complete by EOD"
+              markAsRead={this.markAsRead}
+            />
+            <NotificationItem
+              id={3}
+              type="urgent"
+              html={{ __html: getLatestNotification() }}
+              markAsRead={this.markAsRead}
+            />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
+
+export default Notifications;
