1bcb9e87ad4bdcbd1adf04fe18679aa60da80fbe
root <root@LAPTOP-J7HTNKBB>
2024-02-14 14:39:07 +0100

    3. Reusable comments & specialization

---
+++ React_component/task_3/dashboard/config/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    hot: true,
+  
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+};
---
+++ React_component/task_3/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+test('Notifications renders without crashing', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+test('Notifications renders three NotificationItem elements', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.find('NotificationItem')).toHaveLength(3);
+});
+
+test('Notifications renders the text "Here is the list of notifications"', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.text()).toContain('Here is the list of notifications');
+});
+
+test('Notifications renders "No new notification for now" when listNotifications prop is empty', () => {
+  const wrapper = shallow(<Notifications listNotifications={[]} />);
+  expect(wrapper.text()).toContain('No new notification for now');
+});
+
+test('Notifications renders listNotifications correctly', () => {
+  const listNotifications = [
+    { id: 1, html: { __html: 'Notification 1' }, type: 'default', value: 'New course available' },
+    { id: 2, html: { __html: 'Notification 2' }, type: 'urgent', value: 'Urgent requirement' },
+    { id: 3, html: { __html: 'Notification 3' }, type: 'default', value: 'Another notification' },
+  ];
+  const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
+  const renderedNotifications = wrapper.find('NotificationItem');
+
+  expect(renderedNotifications).toHaveLength(3);
+
+  listNotifications.forEach((notification, index) => {
+    const renderedNotification = renderedNotifications.at(index);
+    expect(renderedNotification.props()).toEqual(notification);
+  });
+});
