e4314831c28849dbacb91e4a1c268d0006c687b8
root <root@LAPTOP-J7HTNKBB>
2024-03-01 09:13:20 +0100

    Modify the setup to access document object

--- 0x08_react_redux_action_creator_normalizr/task_2/dashboard/config/setupTests.js
+++ 0x08_react_redux_action_creator_normalizr/task_2/dashboard/config/setupTests.js
@@ -1,4 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import { JSDOM } from 'jsdom';
+
+const mockDocument = new JSDOM('<!doctype html><html><body></body></html>');
+global.document = mockDocument.window.document;
+
 configure({adapter: new Adapter() });
